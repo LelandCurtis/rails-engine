1b450a8fa35ead78e5e03260b89020a2113290b4
Leland Curtis <lelandcurtis88@gmail.com>
2022-02-07 19:35:11 -0700

    Write validation tests.

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should belong_to(:invoice) }
+    it { should belong_to(:item) }
+    it { should have_many(:transactions).through(:invoices) }
+    it { should have_many(:customers).through(:invoices) }
+    it { should have_many(:merchants).through(:items) }
+  end
 end
