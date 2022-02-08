90d0677814743019e0d5ce36f438313cf6b94e1a
Leland Curtis <lelandcurtis88@gmail.com>
2022-02-07 19:41:10 -0700

    fix typos in validation tests. Add validations.

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
   describe 'validations' do
-    it { should have_many(:invoices) }
-    it { should have_many(:customers).through(:invoices) }
-    it { should have_many(:invoice_items).through(:invoices) }
+    it { should belong_to(:invoice) }
+    it { should have_many(:customers).through(:invoice) }
+    it { should have_many(:invoice_items).through(:invoice) }
     it { should have_many(:items).through(:invoice_items) }
     it { should have_many(:merchants).through(:items) }
   end
